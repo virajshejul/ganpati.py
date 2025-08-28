67e8a8c017bde8a6e87c2aca6541bf1ec9baaac2
virajshejul <vshejul054@gmail.com>
2025-08-28 10:39:34 +0530

    first commit

--- ganpati.py
+++ ganpati.py
@@ -270,3 +270,13 @@ tur.speed(10)  # set the speed
 tur.exitonclick()
 # tur.mainloop()
 # tur.done()
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html") 
+
+if __name__ == "__main__":
+    app.run(debug=True)
